c80e2cc70cdd63ad4945069958ec130e8bc3e8b0
Jzjssje <58160780+Shshshhj@users.noreply.github.com>
2021-12-30 15:02:05 +0530

    Update what_is_my_dc.py

--- Code_X_Mania/bot/plugins/what_is_my_dc.py
+++ Code_X_Mania/bot/plugins/what_is_my_dc.py
@@ -2,7 +2,7 @@ from Code_X_Mania.bot import StreamBot
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-@StreamBot.on_message(filters.regex("dc"))
+@StreamBot.on_message(filters.regex("dujhc"))
 async def start(bot, update):
     text = START_TEXT.format(update.from_user.dc_id)
     await update.reply_text(
